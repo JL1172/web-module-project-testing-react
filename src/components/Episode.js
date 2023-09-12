fb1d1b3fce6167a7cd766d2df725176b534ffc45
Jacob Lang <jacoblang72@comcast.net>
2023-09-12 17:51:27 -0400

    wip

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -9,11 +9,11 @@ const Episode = (props) => {
 
   return (
     <div className="episode" key={id}>
-      <img className="episode-image" src={imgsrc} alt={imgsrc} />
+      <img data-testid = "image" className="episode-image" src={imgsrc} alt={imgsrc} />
       <div className="episode-info">
         <p className="episode-number">Season {season}, Episode {number}</p>
         <h3>{name}</h3>
-        <p>{summary}</p>
+        <p data-testid = "summary">{summary}</p>
         <div className="flex-spacer" />
         <p className="episode-runtime">{runtime} minutes</p>
       </div>
